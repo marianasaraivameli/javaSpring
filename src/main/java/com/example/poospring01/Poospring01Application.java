480905abe880448a38a53d1fef07e871ecb787d5
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-29 20:49:46 -0300

    feat: exemplo - aula ao vivo

---
+++ src/main/java/com/example/poospring01/Poospring01Application.java
@@ -0,0 +1,13 @@
+package com.example.poospring01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poospring01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Poospring01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poospring01/Poospring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poospring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poospring01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
