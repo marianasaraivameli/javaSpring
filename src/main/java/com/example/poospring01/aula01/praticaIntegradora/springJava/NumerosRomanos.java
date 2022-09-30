71c6281677acd6ec5296a5567c7e5a130986c324
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-09-29 21:32:37 -0300

    feat: pratica integradora - exercício 01 - adiciono referencia codigo

--- src/main/java/com/example/poospring01/aula01/praticaIntegradora/springJava/NumerosRomanos.java
+++ src/main/java/com/example/poospring01/aula01/praticaIntegradora/springJava/NumerosRomanos.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/romanos")
 public class NumerosRomanos {
+    //  https://iteracao.info/java-conversao-de-decimais-em-romanos/
     private static final int[] DECIMAIS =
             {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     private static final String[] ROMANOS =
