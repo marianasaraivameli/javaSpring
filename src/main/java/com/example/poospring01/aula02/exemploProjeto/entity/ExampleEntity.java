6155aeb25ac942b2d55faf989d89b95160f2eb6b
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-01 23:04:39 -0300

    feat: modelo - exemplo estrutura de projeto - entity

---
+++ src/main/java/com/example/poospring01/aula02/exemploProjeto/entity/ExampleEntity.java
@@ -0,0 +1,21 @@
+package com.example.poospring01.aula02.exemploProjeto.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//@Entity
+//@Table(name="example")
+public class ExampleEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//
+//    @Column(name= "first_name")
+//    private String fistName;
+//
+//    @Column(name= "email")
+//    private String email;
+}
