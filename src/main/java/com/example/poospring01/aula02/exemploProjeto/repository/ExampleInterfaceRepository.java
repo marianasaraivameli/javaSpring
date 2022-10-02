5568425778a71e1ac73a3712440dc3e81774a041
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-01 23:29:42 -0300

    feat: modelo - exemplo estrutura de projeto - interface repository

---
+++ src/main/java/com/example/poospring01/aula02/exemploProjeto/repository/ExampleInterfaceRepository.java
@@ -0,0 +1,9 @@
+package com.example.poospring01.aula02.exemploProjeto.repository;
+
+import com.example.poospring01.aula02.exemploProjeto.entity.ExampleEntity;
+
+public interface ExampleInterfaceRepository <ExampleEntity, Long> {
+    // https://www.youtube.com/watch?v=aCZmPgBHc88
+    // fazer todo o CRUD aqui
+
+}
